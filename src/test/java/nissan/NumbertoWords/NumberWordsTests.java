56c56d65866eb3bcb40b2cdeb1f265b9964a0f63
Aarathi, Sivaprasad <aarathi-sivaprasad@mail.nissan.co.jp>
2018-10-10 16:52:06 +0530

    first commit done

--- src/test/java/nissan/NumbertoWords/NumberWordsTests.java
+++ src/test/java/nissan/NumbertoWords/NumberWordsTests.java
@@ -30,8 +30,77 @@ public class NumberWordsTests {
 	{
 		
 		NumberWordsApplication obj=new NumberWordsApplication();
-		String output= obj.convert(9999999);
+		String output= obj.convert(99999999);
 		assertEquals("Error Out of range",output);
 		}
-	
+	public void test4()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(10);
+		assertEquals("Ten",output);
+		}
+	public void test5()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(20);
+		assertEquals("Twenty",output);
+		}
+	public void test6()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(16);
+		assertEquals("Sixteen",output);
+		}
+	public void test7()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(22);
+		assertEquals("TwentyTwo",output);
+		}
+	public void test8()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(100);
+		assertEquals("OneHundred",output);
+		}
+	public void test9()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(256);
+		assertEquals("Two Hundred Fifty Six",output);
+		}
+	public void test10()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(903);
+		assertEquals("Nine Hundred Three",output);
+		}
+	public void test11()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(1123);
+		assertEquals("One Thousand One Hundred Twenty Three",output);
+		}
+	public void test12()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(132956);
+		assertEquals("One Lakh Thirty Two Thousand Nine Hundred Fifty Six",output);
+		}
+	public void test13()
+	{
+		
+		NumberWordsApplication obj=new NumberWordsApplication();
+		String output= obj.convert(2000000);
+		assertEquals("Two Crore",output);
+		}
 }
